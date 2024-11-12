54c41e7674d669c76148ca76dc7fb8cb97074ba6
Vaibhav <sidanavaibhav@gmail.com>
2024-11-13 02:58:18 +0530

    centered

--- tailwindBasics/src/components/ProductCatalog.jsx
+++ tailwindBasics/src/components/ProductCatalog.jsx
@@ -2,20 +2,24 @@ import { useEffect } from 'react';
 import Card from './Card.jsx'
 import useFetch from '../Hooks/useFetch.js';
 import Loader from './Loader.jsx';
-function ProductCatalog(){
+function ProductCatalog() {
     const Products = useFetch("https://bocca-mern-gis9.vercel.app/productInfo")
     const prodInfo = Products.products;
     // console.log(Products);
-    return(<>
-        <div className="mt-12 mb-12 grid  place-items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+    return (<>
+        <div className="mt-12 mb-12 grid place-items-center  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
             {prodInfo ? prodInfo.map((product, index) => (
                 <div key={index}>
                     <Card text={product.productName} price={product.productPrice} />
                 </div>
-            )) : 
-                <Loader />
-            }
+            )) :
+            <div className='lg:col-start-2 md:col-start-2' > 
+                    <Loader />
+            </div>
+            
+                        }
         </div>
+
     </>)
 }
 export default ProductCatalog;
