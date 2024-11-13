0da5265a639315f1a50e941e7021ce87ce5f90e9
Vaibhav <sidanavaibhav@gmail.com>
2024-11-14 04:00:56 +0530

    mid

--- tailwindBasics/src/components/Card.jsx
+++ tailwindBasics/src/components/Card.jsx
@@ -3,7 +3,7 @@ function Card(props){
     return(
         <div className="w-306 h-395 grid grid-cols-12 grid grid-rows-12 rounded-2xl bg-white mr-8 mt-4 mb-4"> 
             <button className={`transform-all duration-500 rounded-2xl bg-black hover:bg-yellow-500 col-start-2 col-end-12 row-start-2 row-end-7`}>
-            <img className="w-full h-full" src={props.im}  />
+            <img className="w-[80%] h-[80%] mr-auto ml-auto" src={props.im}  />
             </button>
             <div className="mt-2 col-start-2 col-end-12 row-start-7 row-end-8 poppins text-sm font-bold">
                 {props.text}  
