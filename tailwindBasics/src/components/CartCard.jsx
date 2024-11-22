7a726f0804b1b2fcf3db2f75042d5a78d238d6f2
Vaibhav <sidanavaibhav@gmail.com>
2024-11-22 22:12:36 +0530

    maybe

--- tailwindBasics/src/components/CartCard.jsx
+++ tailwindBasics/src/components/CartCard.jsx
@@ -1,7 +1,7 @@
 function CartCard(props){
     return(
-        <div className="flex flex-row align-center content-center items-center ml-4  w-[100%] h-20 border-2 border-black bg-red-200">
-            <img src={props.im} alt="image address" className="w-[20%] h-[80%]" />
+        <div className="flex flex-row align-center content-center items-center ml-4 overflow-y-hidden w-[100%] h-20 border-2 border-black bg-red-200">
+            <img src={props.im} alt="image address" className="w-[15%] h-[80%]" />
         </div>
     )
 }
