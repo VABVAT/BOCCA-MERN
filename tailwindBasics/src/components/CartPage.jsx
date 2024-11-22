860d73c0bf352e7ff9932ec5c0895da3f7660cda
Vaibhav <sidanavaibhav@gmail.com>
2024-11-23 00:52:24 +0530

    cart initiated

--- tailwindBasics/src/components/CartPage.jsx
+++ tailwindBasics/src/components/CartPage.jsx
@@ -2,12 +2,13 @@ import { useNavigate } from "react-router-dom"
 import CartCard from "./CartCard"
 import la from '/left-arrow.png'
 import p1 from '/p1.png'
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import Loader from "./Loader";
 function CartPage(){
     const navigate = useNavigate();
     const [prods, setProds] = useState();
+    const loader = useRef(false)
     useEffect(() => {
         const interval = setInterval(() => {
             const token = localStorage.getItem('token')
@@ -30,6 +31,7 @@ function CartPage(){
                 })
             })
             const response = await data.json()
+            loader.current = true;
             setProds(response.cart)  
         }
         getItems()        
@@ -44,7 +46,7 @@ function CartPage(){
         </button>
     </div>
     <div className="col-start-4 col-end-9 overflow-x-hidden   row-start-4 row-end-10">
-    {prods ? prods.map((curr) => (curr ? <CartCard im={curr.image} text={curr.name} price={curr.price}/> : null)) : <Loader />}
+    {loader.current ? prods.map((curr) => (curr ? <CartCard im={curr.image} text={curr.name} price={curr.price}/> : null)) : <Loader />}
     </div>
     
     
