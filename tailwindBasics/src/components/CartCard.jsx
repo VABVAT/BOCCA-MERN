cce894be0f5d54c885ac1074c8cd499ff1a5c3da
Vaibhav <sidanavaibhav@gmail.com>
2024-11-22 23:03:02 +0530

    correction

--- tailwindBasics/src/components/CartCard.jsx
+++ tailwindBasics/src/components/CartCard.jsx
@@ -1,6 +1,6 @@
 function CartCard(props){
     return(
-        <div className="flex flex-row align-center content-center items-center ml-4 overflow-y-hidden w-[100%] h-20 border-2 border-black bg-red-200">
+        <div className="flex flex-row align-center content-center items-center ml-4 overflow-y-hidden w-[95%] h-20 border-2 border-black bg-red-200">
             <img src={props.im} alt="image address" className="w-[15%] h-[80%]" />
         </div>
     )
